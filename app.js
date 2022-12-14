0fbbbe04235c124be7d83d4de1e38fc1d83494f2
jaykulkarni2620 <kulkarnijay76@gmail.com>
2022-12-14 13:27:13 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  console.log("hello");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/home", function (req, res) {
+  console.log(req);
+  console.log("my name is");
+});
+
+// const url =
+//   "https://api.openweathermap.org/data/2.5/weather?q=india,mumbai&appid=6c2ac87614d4e72bd9888c7c60d3a91a&units=metric";
+// https.get(url, function (response) {
+//   console.log(response.statusCode);
+
+//   response.on("data", function (data) {
+//     const weatherData = JSON.parse(data);
+//     const temp = weatherData.main.temp;
+//     const icon = weatherData.weather[0].icon;
+//     const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+//     console.log(weatherData);
+//     console.log(temp);
+//     console.log(icon);
+//     // res.send("the weather is ");
+//   });
+// });
+
+app.listen(3200, () => console.log("call on port 3200..."));
