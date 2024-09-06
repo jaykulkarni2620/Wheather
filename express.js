a8d3c7a25a16578342ce09d5889e1e2495ad213b
Jay Kulkarni <88571223+jaykulkarni2620@users.noreply.github.com>
2024-09-06 16:27:30 +0530

    Update express.js

--- express.js
+++ express.js
@@ -1,31 +1,50 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
+// Serve the main HTML file
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post("/hello", function (req, res) {
-  res.send("hello world");
-});
+// Handle weather requests
+app.get("/weather", function (req, res) {
+  const cityName = req.query.city;
+  const apiKey = '6c2ac87614d4e72bd9888c7c60d3a91a'; // Replace with your API key
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
-const url =
-  "https://api.openweathermap.org/data/2.5/weather?q=india,mumbai&appid=6c2ac87614d4e72bd9888c7c60d3a91a&units=metric";
-https.get(url, function (response) {
-  console.log(response.statusCode);
+  https.get(url, function (response) {
+    let data = '';
 
-  response.on("data", function (data) {
-    const weatherData = JSON.parse(data);
-    const temp = weatherData.main.temp;
-    const icon = weatherData.weather[0].icon;
-    const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-    console.log(weatherData);
-    console.log(temp);
-    console.log(icon);
+    response.on('data', chunk => data += chunk);
+
+    response.on('end', () => {
+      const weatherData = JSON.parse(data);
+      if (weatherData.cod === '404') {
+        res.json({ error: 'City not found' });
+      } else {
+        const temp = weatherData.main.temp;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+        res.json({
+          name: weatherData.name,
+          temp: temp,
+          iconURL: imageURL
+        });
+      }
+    });
+  }).on('error', (e) => {
+    console.error(e);
+    res.json({ error: 'Error fetching weather data' });
   });
 });
-// res.send("the weather is ");
 
-app.listen(3200, () => console.log("call on port 3200..."));
+app.listen(3200, () => console.log("Server running on port 3200"));
